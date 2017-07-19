3c62bfa84cd33caa1a0ada4842d2f81451e4bc4b
Michael Rans <rans@email.com>
2017-07-19 17:34:57 +0200

    crontab

--- run.py
+++ run.py
@@ -48,4 +48,6 @@ def main():
             showcase.add_dataset(dataset)
 
 if __name__ == '__main__':
-    facade(main, hdx_site='feature', project_config_yaml=join('config', 'project_configuration.yml'))
+    facade(main, hdx_site='feature', project_config_yaml=join('config', 'project_configuration.yml'),
+           smtp_config_yaml=join('config', 'smtp_configuration.yml'))
+
