f352f85b2ff49e776fea2aa6c8d6d4caff340c6e
Michael Rans <rans@email.com>
2017-07-20 07:07:00 +0200

    logging args

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ from os.path import join, expanduser
 from tempfile import gettempdir
 
 from hdx.hdx_configuration import Configuration
+from hdx.facades import logging_kwargs
 from hdx.facades.hdx_scraperwiki import facade
 from hdx.utilities.downloader import Download
 
@@ -48,6 +49,6 @@ def main():
             showcase.add_dataset(dataset)
 
 if __name__ == '__main__':
-    facade(main, hdx_site='feature', project_config_yaml=join('config', 'project_configuration.yml'),
-           smtp_config_yaml=join('config', 'smtp_configuration.yml'))
+    logging_kwargs.update(smtp_config_yaml=join('config', 'smtp_configuration.yml'))
+    facade(main, hdx_site='feature', project_config_yaml=join('config', 'project_configuration.yml'))
 
