3f74a6e54d8c7288e326bb15c913d23d897806af
Mike <rans@email.com>
2018-11-26 13:01:44 +0100

    Use temporary folder utility

--- run.py
+++ run.py
@@ -33,39 +33,36 @@ lookup = 'hdxscraper-fts'
 def main():
     '''Generate dataset and create it in HDX'''
 
-    base_url = Configuration.read()['base_url']
-    with Download(extra_params_yaml=join(expanduser('~'), '.extraparams.yml'), extra_params_lookup=lookup) as downloader:
-        clusters = get_clusters(base_url, downloader)
-        countries = get_countries(base_url, downloader)
-        folder = join(temp_dir(), 'fts')
-        if not exists(folder):
-            mkdir(folder)
-        today = datetime.now()
-        for country in countries:
-            locationid = country['id']
-            countryname = country['name']
-            if countryname == 'World':
-                logger.info('Ignoring  %s' % countryname)
-                continue
-            logger.info('Adding FTS data for %s' % countryname)
-            dataset, showcase, hxl_resource = generate_dataset_and_showcase(base_url, downloader, folder, clusters, country['iso3'], countryname, locationid, today)
-            if dataset is None:
-                logger.info('No data for %s' % countryname)
-            else:
-                dataset.update_from_yaml()
-                if hxl_resource is None:
-                    dataset.preview_off()
-                    hxl_update = False
+    with temp_dir('fts') as folder:
+        with Download(extra_params_yaml=join(expanduser('~'), '.extraparams.yml'), extra_params_lookup=lookup) as downloader:
+            base_url = Configuration.read()['base_url']
+            clusters = get_clusters(base_url, downloader)
+            countries = get_countries(base_url, downloader)
+            today = datetime.now()
+            for country in countries:
+                locationid = country['id']
+                countryname = country['name']
+                if countryname == 'World':
+                    logger.info('Ignoring  %s' % countryname)
+                    continue
+                logger.info('Adding FTS data for %s' % countryname)
+                dataset, showcase, hxl_resource = generate_dataset_and_showcase(base_url, downloader, folder, clusters, country['iso3'], countryname, locationid, today)
+                if dataset is None:
+                    logger.info('No data for %s' % countryname)
                 else:
-                    dataset.set_quickchart_resource(hxl_resource)
-                    hxl_update = True
-                dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False)
-                resources = dataset.get_resources()
-                resource_ids = [x['id'] for x in sorted(resources, key=lambda x: len(x['name']), reverse=True)]
-                dataset.reorder_resources(resource_ids, hxl_update=hxl_update)
-                showcase.create_in_hdx()
-                showcase.add_dataset(dataset)
-        rmtree(folder)
+                    dataset.update_from_yaml()
+                    if hxl_resource is None:
+                        dataset.preview_off()
+                        hxl_update = False
+                    else:
+                        dataset.set_quickchart_resource(hxl_resource)
+                        hxl_update = True
+                    dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False)
+                    resources = dataset.get_resources()
+                    resource_ids = [x['id'] for x in sorted(resources, key=lambda x: len(x['name']), reverse=True)]
+                    dataset.reorder_resources(resource_ids, hxl_update=hxl_update)
+                    showcase.create_in_hdx()
+                    showcase.add_dataset(dataset)
 
 
 if __name__ == '__main__':
