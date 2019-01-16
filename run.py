ed34a19175e7ae297436a3d3f70e3fc57ee84d54
Mike <rans@email.com>
2019-01-16 12:55:06 +0100

    Update to not keep temp dir

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ lookup = 'hdx-scraper-fts'
 def main():
     '''Generate dataset and create it in HDX'''
 
-    with temp_dir('fts', False) as folder:
+    with temp_dir('fts') as folder:
         with Download(extra_params_yaml=join(expanduser('~'), '.extraparams.yml'), extra_params_lookup=lookup) as downloader:
             base_url = Configuration.read()['base_url']
             countries = get_countries(base_url, downloader)
